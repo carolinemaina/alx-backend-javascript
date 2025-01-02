fc0c5ce91287daaf94247b6e24625cf4a52daf4c
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-01-02 12:47:06 +0000

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayValue = [];
+  for (const value of array) {
+    arrayValue.push(appendString + value);
+  }
+
+  return array;
+}
