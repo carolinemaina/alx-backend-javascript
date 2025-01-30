28eacf347004ef6c06851785e89884d90d9a8935
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-01-30 19:10:36 +0300

    change name

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 require('process');
 
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Start reading from stdin
 process.stdin.resume();
