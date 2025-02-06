e0c3b7d3f9faf69b3f6ba1b88c8ffbc72450ca50
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-06 06:07:19 +0300

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,18 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('Rounds and adds two numbers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(2.5, 3.3), 6);
   });
-  it('Round a number and decimal', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('Round 0 and a positive number', () => {
+    assert.strictEqual(calculateNumber(0, 5), 5);
   });
-  it('Round and add two decimals', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('Round 0 and a negative number', () => {
+    assert.strictEqual(calculateNumber(0, -2), -2);
   });
-  it('Round and add two decimals', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('Round two negative numbers', () => {
+    assert.strictEqual(calculateNumber(-2.4, -3.7), -6);
+  });
+  it('Round one positive and one negative number', () => {
+    assert.strictEqual(calculateNumber(3.2, -1.8), 1);
   });
 });
